49811d9ed57aff5fc0ca5d28171ea7b99cca682d
Berna232008 <b2kz78ftkm@privaterelay.appleid.com>
2026-09-21 21:55:22 +0100

    Initial Commit

---
+++ SwipecarPAP/SwipecarPAPApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipecarPAPApp.swift
+//  SwipecarPAP
+//
+//  Created by Bernardo Simoes on 21/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct SwipecarPAPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
